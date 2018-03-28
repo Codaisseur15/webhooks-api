e56723b87ecab1b8e0e32b42f58963c1f7b34345
FStriewski <fstriewski@outlook.com>
2018-03-28 10:54:58 +0200

    Move index content to app, create new test files for controllers

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import 'reflect-metadata'
+import { createKoaServer } from "routing-controllers"
+import setupDb from './db'
+import TargetController from "./targeturls/controller";
+import EventController from "./events/controller";
+
+const port = process.env.PORT || 4008
+
+export const app = createKoaServer({
+    controllers: [
+        TargetController,
+        EventController
+    ]
+})
+
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,9 @@
-import 'reflect-metadata'
-import {createKoaServer} from "routing-controllers"
 import setupDb from './db'
-import TargetController from "./targeturls/controller";
-import EventController from "./events/controller";
+import {app} from './app'
 
-const port = process.env.PORT || 4008
-
-const app = createKoaServer({
-  controllers: [
-    TargetController,
-    EventController
-  ]
-})
 
 setupDb()
-  .then(_ => {
-    app.listen(port, () => console.log(`Listening on port ${port}`))
-  })
+  .then(_ =>
+    app.listen(4008, () => console.log('Listening on port 4008'))
+  )
   .catch(err => console.error(err))
