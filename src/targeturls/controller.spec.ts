e4562334f5abfc4437b6deab41f7630e2c58d012
FStriewski <fstriewski@outlook.com>
2018-03-28 15:20:27 +0200

    Add get/id test

--- src/targeturls/controller.spec.ts
+++ src/targeturls/controller.spec.ts
@@ -5,11 +5,13 @@ import * as request from 'supertest'
 import {app} from '../app'
 import setupDb from '../db'
 
+// waits for promise being resolved before running any of the tests
 beforeAll(async () => {
     await setupDb()
 })
 
 describe('TargetController', () => {
+    
     test('/targets', async () => {
         await request(await app.callback())
             .get('/targets')
@@ -17,4 +19,35 @@ describe('TargetController', () => {
             //.set('x-user-roles', 'teacher')
             .expect(200)
     })
+
+    test('/targets/:id', async () => {
+        await request(await app.callback())
+            .get('/targets/:id')
+            .set('Accept', 'application/json')
+            //.set('x-user-roles', 'teacher')
+            .expect(function (res) {
+                res.body.id = 1;
+            })
+            .expect(200, {
+                id: 1
+            })
+    })
+
 })
+
+/*
+describe('GET /user', function() {
+  it('user.name should be an case-insensitive match for "tobi"', function(done) {
+    request(app)
+      .get('/user')
+      .set('Accept', 'application/json')
+      .expect(function(res) {
+        res.body.id = 'some fixed id';
+        res.body.name = res.body.name.toUpperCase();
+      })
+      .expect(200, {
+        id: 'some fixed id',
+        name: 'TOBI'
+      }, done);
+  });
+});*/
