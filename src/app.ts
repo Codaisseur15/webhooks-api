205984348a5a29fa088630a8a0159b4fe62c75a8
FStriewski <fstriewski@outlook.com>
2018-03-28 12:35:15 +0200

    Comment out port in app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { createKoaServer } from "routing-controllers"
 import TargetController from "./targeturls/controller";
 import EventController from "./events/controller";
 
-const port = process.env.PORT || 4008
+//const port = process.env.PORT || 4008
 
 export const app = createKoaServer({
     controllers: [
